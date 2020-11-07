5e2d6cd4dba860410efa4790426060b6b636e042
farwolf2010 <362675035@qq.com>
2020-11-07 23:56:15 +0800

    update

--- android/office/src/main/java/com/office/plugin/init/PluginInit.java
+++ android/office/src/main/java/com/office/plugin/init/PluginInit.java
@@ -1,10 +1,12 @@
 package com.office.plugin.init;
 
 import android.content.Context;
+import android.util.Log;
 
 import com.farwolf.weex.annotation.ModuleEntry;
 import com.office.plugin.util.ExceptionHandler;
 import com.tencent.smtt.sdk.QbSdk;
+
 
 //此注解会被框架扫描到并执行类的init方法
 @ModuleEntry
@@ -16,8 +18,22 @@ public class PluginInit {
     public  void init(Context context)
     {
 
-        QbSdk.initX5Environment(context,null);
-        ExceptionHandler.getInstance().initConfig(context);
+        QbSdk.PreInitCallback cb = new QbSdk.PreInitCallback() {
+
+            @Override
+            public void onViewInitFinished(boolean arg0) {
+                // TODO Auto-generated method stub
+                //x5內核初始化完成的回调，为true表示x5内核加载成功，否则表示x5内核加载失败，会自动切换到系统内核。
+                Log.d("app", " onViewInitFinished is " + arg0);
+            }
+
+            @Override
+            public void onCoreInitFinished() {
+                // TODO Auto-generated method stub
+            }
+        };
+        //x5内核初始化接口
+        QbSdk.initX5Environment(context,  cb);
 
     }
 }
